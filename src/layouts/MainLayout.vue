ce52f77d86e607066472cd12d66bd7ed2168c458
Jamil Mariano <marianojamil15@gmail.com>
2026-05-17 21:30:18 +0800

    Update layout and styles, add TechnologyPage, and modify IndexPage structure

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,24 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+      <q-toolbar class="bg-white">
+        <q-img
+          src="src/assets/logo.png"
+          class="logo"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-space />
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn-toggle
+          v-model="btnRef"
+          color="primary"
+          flat
+          stretch
+          toggle-color="secondary"
+          :options="options"
+        />
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -47,56 +27,18 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
 
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+const btnRef = ref('Home');
+
+const options = [
+  { label: 'Home', value: 'Home' },
+  { label: 'Globalization', value: 'Globalization' },
+  { label: 'Areas of Impact', value: 'Areas of Impact' },
+  { label: 'Meet the Team', value: 'Meet the Team' },
 ];
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
 </script>
+
+<style scoped lang="sass">
+.logo
+  width: clamp(80px, 15vw, 120px)
+</style>
---
+++ src/pages/TechnologyPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <div>
+      Hello World
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+
+</style>
